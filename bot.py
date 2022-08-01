31dcfa72c0794b156828b44fac811e1145f97b47
Dinislam <dinislamsayfullin@gmail.com>
2022-08-01 11:24:55 +0500

    Add base bot logic

---
+++ bot.py
@@ -0,0 +1,44 @@
+from aiogram import Dispatcher, Bot, types
+from aiogram.utils.executor import start_webhook
+
+import logging
+import os
+
+logging.basicConfig(level=logging.INFO)
+
+TOKEN = os.environ.get('TOKEN')
+HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
+
+WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
+WEBHOOK_PATH = f'/webhook/{TOKEN}'
+WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
+
+WEBAPP_HOST = '0.0.0.0'
+WEBAPP_PORT = os.getenv('PORT', default=5000)
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+# botDB = BotDB(os.environ.get('DB_URI'))
+
+
+async def on_startup(dispatcher):
+    await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
+
+
+async def on_shutdown(dispatcher):
+    await bot.delete_webhook()
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    await message.answer(f'Привет, {message.from_user.first_name}')
+
+
+if __name__ == '__main__':
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        skip_updates=True,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
+    )
